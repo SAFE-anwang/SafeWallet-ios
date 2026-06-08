58bf30b3ee63dc87accae9fb08facb54c30a9a23
蒋松伦 <271398741@qq.com>
2026-06-08 20:35:23 +0800

    Tweak KLine indicator theme styles

--- UnstoppableWallet/UnstoppableWallet/Modules/KLine/KLineSwiftUIView.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/KLine/KLineSwiftUIView.swift
@@ -100,16 +100,10 @@ extension KLineConfiguration.ThemePreset {
             .wma(5): LineStyle(strokeColor: UIColor(red: 0.63, green: 0.89, blue: 0.75, alpha: 1)),
             .wma(10): LineStyle(strokeColor: UIColor(red: 0.74, green: 0.8, blue: 0.98, alpha: 1)),
             .wma(20): LineStyle(strokeColor: UIColor(red: 0.97, green: 0.84, blue: 0.96, alpha: 1)),
-            .boll: LineStyle(strokeColor: UIColor(red: 0.64, green: 0.78, blue: 1, alpha: 1)),
             .sar: LineStyle(strokeColor: UIColor(red: 1, green: 0.71, blue: 0.45, alpha: 1)),
             .rsi(6): LineStyle(strokeColor: UIColor(red: 0.83, green: 0.93, blue: 1, alpha: 1)),
             .rsi(12): LineStyle(strokeColor: UIColor(red: 1, green: 0.77, blue: 0.53, alpha: 1)),
             .rsi(24): LineStyle(strokeColor: UIColor(red: 0.58, green: 0.89, blue: 0.71, alpha: 1)),
-            .macd: MACDStyle(
-                macdColor: UIColor(red: 0.97, green: 0.69, blue: 0.36, alpha: 1),
-                difColor: UIColor(red: 0.63, green: 0.89, blue: 1, alpha: 1),
-                deaColor: UIColor(red: 1, green: 0.47, blue: 0.64, alpha: 1)
-            )
         ]
         return KLineConfiguration.ThemePreset(
             candleStyle: candle,
